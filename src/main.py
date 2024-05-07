f6fcd157138b98db54bb10c36b8ba79d34b95517
Sarguru02 <psgnhealth@gmail.com>
2024-05-07 16:31:36 +0530

    playing with websockets and game management

--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
-from game.board import Board
-from game.property import Property
-from game.property import Player
+import asyncio
+import websockets
 
+class NetworkManager:
+    def __init__(self):
+        self.connections = []
+        self.game = None
+        self.started = asyncio.Event()
+        self.counter = 0
 
-def main():
-    board = Board()
-    player = Player("Player 1")
-    property = Property("Property 1", 1000)
+    async def handle_connection(self, websocket):
+        self.connections.append(websocket)
+        websockets.broadcast(self.connections, "New player connected!")
+        if len(self.connections) >= 2:
+            await self.start_game(self.connections[:2])
+            self.connections = self.connections[2:]
+        async for message in websocket:
+            print(f"Received message: {message}")
+            await websocket.send("Hello, world!")
+
+    async def start_game(self, connections):
+        self.counter += 1
+        self.game = connections
+        websockets.broadcast(connections, f"Game{self.counter} started!")
+
+async def main():
+    network_manager = NetworkManager()
+    async with websockets.serve(network_manager.handle_connection, "localhost", 42069):
+        await asyncio.Future()
+
+asyncio.run(main())
